860da98a2b8eebc5bf2c03d99d2afd85b0763dfd
TulsiLukhi1 <lukhitulshi20137@gmail.com>
2023-08-04 15:16:45 +0530

    test for moveforward,up,right

--- src/test/java/spacecraft/Chandrayaan3.java
+++ src/test/java/spacecraft/Chandrayaan3.java
@@ -26,6 +26,65 @@ public class Chandrayaan3 {
     public Direction getDirection() {
         return direction;
     }
+    
+    
+    public void moveForward() {
+    	
+        switch (direction) {
+            case NORTH :
+            case SOUTH:
+                y++;
+                break;
+            case EAST:
+            case WEST:
+                x++;
+                break;
+            case UP:
+            case DOWN:
+                z++;
+                break;
+          
+        }
+        
+        
+    }
+    
+    public void moveRight() {
+        switch (direction) {
+            case NORTH:
+                direction = Direction.EAST;
+                break;
+            case EAST:
+                direction = Direction.SOUTH;
+                break;
+            case SOUTH:
+                direction = Direction.WEST;
+                break;
+            case WEST:
+                direction = Direction.NORTH;
+                break;
+            default:
+                // Up and Down directions don't change when moving right
+                break;
+        }
+    }
+    
+    
+    public void moveUp() {
+        switch (direction) {
+            case NORTH:
+            case SOUTH:
+            case EAST:
+            case WEST:
+                direction = Direction.UP;
+                break;
+            default:
+                // Up and Down directions are already facing up or down
+                break;
+        }
+    }
+
+
 
     
 }
--- src/test/java/spacecraft/Chandrayaan3Test.java
+++ src/test/java/spacecraft/Chandrayaan3Test.java
@@ -12,4 +12,39 @@ public class Chandrayaan3Test {
         assertEquals(Direction.NORTH, spacecraft.getDirection());
 
 	}
+	
+	//test to move forward
+	@Test
+	public void testMoveForward() {
+	    Chandrayaan3 spacecraft = new Chandrayaan3(0, 0, 0, Direction.NORTH);
+	    spacecraft.moveForward();
+	    
+	    assertEquals(0, spacecraft.getX());
+	    assertEquals(1, spacecraft.getY());
+	    assertEquals(0, spacecraft.getZ());
+	    assertEquals(Direction.NORTH,spacecraft.getDirection());
+	}
+	
+	@Test
+	public void testMoveRight() {
+		Chandrayaan3 spacecraft = new Chandrayaan3(0, 1, 0, Direction.NORTH);
+	    spacecraft.moveRight();
+	    
+	    assertEquals(0, spacecraft.getX());
+	    assertEquals(1, spacecraft.getY());
+	    assertEquals(0, spacecraft.getZ());
+	    assertEquals(Direction.EAST,spacecraft.getDirection());
+	}
+	
+	@Test
+	public void testMoveUp() {
+		Chandrayaan3 spacecraft = new Chandrayaan3(0, 1, 0, Direction.UP);
+	    spacecraft.moveUp();
+	    
+	    assertEquals(0, spacecraft.getX());
+	    assertEquals(1, spacecraft.getY());
+	    assertEquals(0, spacecraft.getZ());
+	    assertEquals(Direction.UP,spacecraft.getDirection());
+	}
+
 }
